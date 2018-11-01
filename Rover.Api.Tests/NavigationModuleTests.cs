35b68c31c188936ae64104f02650c508bc0be716
GreenyMcDuff <chris.greenwood1984@outlook.com>
2018-11-01 11:29:51 +0000

    Modified some old tests after realising -ve coords wasn't a condition to test for

--- Rover.Api.Tests/NavigationModuleTests.cs
+++ Rover.Api.Tests/NavigationModuleTests.cs
@@ -52,16 +52,17 @@ namespace Rover.Api.Tests
 
         [Theory]
         [InlineData(0, 0, 'N', 'F', 0, 1, 'N')]
-        [InlineData(0, 0, 'N', 'B', 0, -1, 'N')]
+        [InlineData(0, 0, 'N', 'B', 0, 100, 'N')]
         [InlineData(0, 0, 'E', 'F', 1, 0, 'E')]
-        [InlineData(0, 0, 'E', 'B', -1, 0, 'E')]
-        [InlineData(0, 0, 'S', 'F', 0, -1, 'S')]
+        [InlineData(0, 0, 'E', 'B', 100, 0, 'E')]
+        [InlineData(0, 0, 'S', 'F', 0, 100, 'S')]
         [InlineData(0, 0, 'S', 'B', 0, 1, 'S')]
-        [InlineData(0, 0, 'W', 'F', -1, 0, 'W')]
+        [InlineData(0, 0, 'W', 'F', 100, 0, 'W')]
         [InlineData(0, 0, 'W', 'B', 1, 0, 'W')]
         [InlineData(0, 100, 'N', 'F', 0, 0, 'N')]
         [InlineData(100, 0, 'E', 'F', 0, 0, 'E')]
-        [InlineData(0, 0, 'N', 'B', 0, 100, 'N')]
+        [InlineData(100, 100, 'N', 'F', 100, 0, 'N')]
+        [InlineData(100, 100, 'E', 'F', 0, 100, 'E')]
         public void Should_MoveInCorrectDirection(int beginX, int beginY, char beginDirection, char command,
             int expectedX, int expectedY, char expectedDirection)
         {
