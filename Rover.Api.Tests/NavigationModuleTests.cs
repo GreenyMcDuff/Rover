685afa1e14a634f65f78c29c976a35b9dfa0b658
GreenyMcDuff <chris.greenwood1984@outlook.com>
2018-11-01 10:37:51 +0000

    Additional test cases implemented

--- Rover.Api.Tests/NavigationModuleTests.cs
+++ Rover.Api.Tests/NavigationModuleTests.cs
@@ -8,6 +8,13 @@ namespace Rover.Api.Tests
     {
         [Theory]
         [InlineData(0, 0, 'N', 'F', 0, 1, 'N')]
+        [InlineData(0, 0, 'N', 'B', 0, -1, 'N')]
+        [InlineData(0, 0, 'E', 'F', 1, 0, 'E')]
+        [InlineData(0, 0, 'E', 'B', -1, 0, 'E')]
+        [InlineData(0, 0, 'S', 'F', 0, -1, 'S')]
+        [InlineData(0, 0, 'S', 'B', 0, 1, 'S')]
+        [InlineData(0, 0, 'W', 'F', -1, 0, 'W')]
+        [InlineData(0, 0, 'W', 'B', 1, 0, 'W')]
         public void Should_MoveInCorrectDirection(int beginX, int beginY, char beginDirection, char command,
             int expectedX, int expectedY, char expectedDirection)
         {
